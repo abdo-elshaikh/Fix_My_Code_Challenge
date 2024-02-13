17ccfe9c02ab5041de349726a0a954aaa320aae5
abdo-elshaikh <abdo_mhmd1@hotmail.com>
2024-02-13 17:48:16 +0200

    fix square class

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
 
-class square():
-    
+class Square():
+    """ model"""
     width = 0
     height = 0
 
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.width * self.height
 
-    def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+    def PerimeterOfMySquare(self):
+        """ Perimeter of the square """
+        return 2 * (self.width + self.height)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.PerimeterOfMySquare())
